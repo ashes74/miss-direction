116ae5f044527d15637cb828db0491d1255accb9
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-08-13 15:25:51 -0400

    What is happening

--- modules/places-search.js
+++ modules/places-search.js
@@ -19,8 +19,22 @@ function initAutocomplete() {
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
         searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+          var theRealPlaces = searchBox.getPlaces();
+          //we're getting trickay -- copied and altered real places
+          var places = [Object.create(theRealPlaces[0])];
+          places[0].geometry.location.lat = function() {return 40.8091005};
+          places[0].geometry.location.lng = function() {return -73.9639386};
 
+          places[0].geometry.viewport.getCenter = function(){
+            return {
+              lat: function() {return 40.8091005}, 
+              lng: function() {return -73.9639386}
+            }
+          }
+
+          console.log('CENTER! ', places[0].geometry.viewport.getCenter());
+          //no more tricks
+          console.log("places", places);
           if (places.length == 0) {
             return;
           }
@@ -46,15 +60,38 @@ function initAutocomplete() {
               scaledSize: new google.maps.Size(25, 25)
             };
 
+            // Randomly miss-direct
+            // var missDirect = function(lat, lng) {
+            //   var missDegrees = (Math.random()/1000) * 5;
+            //   var sign = function() {
+            //     var prob = (Math.random()/1000) * 5;
+            //     if (prob > 0.5) return 
+            //   }
+            // }
+            
             // Create a marker for each place.
             markers.push(new google.maps.Marker({
               map: map,
               icon: icon,
               title: place.name,
               position: place.geometry.location
+              //giza?
+              // position: {
+              //   lat: function(){
+              //     return 28.766621;
+              //   },
+              //   lng: function(){
+              //     return 29.232078399999978;
+              //   }
+              // }
             }));
 
-            console.log('SEARCHED LOCATION ', markers.position);
+            // console.log('SEARCHED LOCATION ', place.geometry.location.toJSON());
+
+            //place.geometry.location.lat(30)
+
+            // console.log('SEARCHED LOCATION ', place.geometry.location.lat(30) );
+           // console.log(place.geometry.location.lat(function() {return 30}))
 
             if (place.geometry.viewport) {
               // Only geocodes have viewport.
@@ -66,3 +103,6 @@ function initAutocomplete() {
           map.fitBounds(bounds);
         });
       }
+
+
+
